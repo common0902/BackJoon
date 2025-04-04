3ac6816ec55d7170e6f76bf8c1d5b22aaa24ba5c
common0902 <minsoung200@gmail.com>
2025-04-04 12:17:40 +0900

    [Bronze IV] Title: 주사위 세개, Time: 48 ms, Memory: 5480 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.cs
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cs
@@ -1,21 +1,37 @@
-int[] dice = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+namespace CS_250404
+{
+    internal class Program
+    {
+        static int FindMax(int x, int y, int z)
+        {
+            int max = x;
+            if (max < y)
+                max = y;
+            if (max < z)
+                max = z;
 
-if (dice[0] == dice[1] && dice[1] == dice[2])
-{
-    Console.WriteLine(10000 + dice[0] * 1000);
-}
-else if (dice[0] == dice[1] || dice[0] == dice[2])
-{
-    Console.WriteLine(1000 + dice[0] * 100);
-}
-else if (dice[1] == dice[2])
-    Console.WriteLine(1000 + dice[1] * 100);
-else
-{
-    int max = dice[0];
-    if (max < dice[1])
-        max = dice[1];
-    if (max < dice[2])
-        max = dice[2];
-    Console.WriteLine(max * 100);
+            return max;
+        }
+        static void Main(string[] args)
+        {
+            int[] dice = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            if (dice[0] == dice[1] && dice[1] == dice[2])
+            {
+                Console.WriteLine(10000 + dice[0] * 1000);
+            }
+            else if (dice[0] == dice[1] || dice[0] == dice[2])
+            {
+                Console.WriteLine(1000 + dice[0] * 100);
+            }
+            else if (dice[1] == dice[2])
+            {
+                Console.WriteLine(1000 + dice[1] * 100);
+            }
+            else
+            {
+                Console.WriteLine(FindMax(dice[0], dice[1], dice[2]) * 100);
+            }
+        }
+    }
 }
