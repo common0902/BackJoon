c263eca257378e6eb185cb1c2f850ab39f6d8d30
common0902 <minsoung200@gmail.com>
2025-04-15 23:06:39 +0900

    [Bronze V] Title: 오늘 날짜, Time: 48 ms, Memory: 5552 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cs
@@ -0,0 +1,12 @@
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dateTime = DateTime.Now.ToString("yyyy-MM-dd");
+
+            Console.WriteLine(dateTime);
+        }
+    }
+}
