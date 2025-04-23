006422355083da5929b021a2015b32232c8445f0
common0902 <minsoung200@gmail.com>
2025-04-24 00:03:13 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 204 ms, Memory: 6572 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cs
@@ -0,0 +1,11 @@
+int a = int.Parse(Console.ReadLine());
+for (int i = 0; i < a; i++)
+{
+    for (int j = a; j >  i; j--)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine(" ");
+
+
+}
