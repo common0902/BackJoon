4f96a3b9d7a79adb3f358d535237caac380cd47a
common0902 <minsoung200@gmail.com>
2025-05-09 08:32:13 +0900

    [Bronze V] Title: A+B - 8, Time: 60 ms, Memory: 6100 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cs
@@ -0,0 +1,11 @@
+int a = int.Parse(Console.ReadLine());
+
+for (int i = 1; i < a + 1; i++)
+{
+    int[] bc = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+    int b = bc[0];
+    int c = bc[1];
+
+    Console.WriteLine($"Case #{i}: {b} + {c} = {b + c}");
+}
