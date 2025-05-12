296e276e14965f37cd0309bc707e220f1970afd4
common0902 <minsoung200@gmail.com>
2025-05-12 09:02:31 +0900

    [Bronze IV] Title: 평균 점수, Time: 48 ms, Memory: 5464 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.cs
@@ -0,0 +1,22 @@
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int d = int.Parse(Console.ReadLine());
+int e = int.Parse(Console.ReadLine());
+
+if (a < 40)
+    a = 40;
+if (b < 40)
+    b = 40;
+if (c < 40)
+    c = 40;
+if (d < 40)
+    d = 40;
+if (e < 40)
+    e = 40;
+
+
+int num = 0;
+
+num = a + b + c + d + e;
+Console.WriteLine(num/5);
