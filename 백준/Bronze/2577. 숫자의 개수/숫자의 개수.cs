47cf870e594b9b831c404250134093c339db7afc
common0902 <minsoung200@gmail.com>
2025-07-20 22:25:59 +0900

    [Bronze II] Title: 숫자의 개수, Time: 52 ms, Memory: 5752 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cs
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cs
@@ -1,35 +1,19 @@
-using System.Numerics;
-using System.Runtime.Serialization;
-using System.Text;
+int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+int C = int.Parse(Console.ReadLine());
 
-namespace ConsoleApp4
+int x = A * B * C;
+string X = x.ToString();
+    
+for (int i = 0; i < 10; i++)
 {
-    internal class Program
-    { 
-        static void Main(string[] args)
-        {
-            int A = int.Parse(Console.ReadLine());
-            int B = int.Parse(Console.ReadLine());
-            int C = int.Parse(Console.ReadLine());
+    int count = 0;
+    string I = i.ToString();
 
-            int x = A * B * C;
-            string X = x.ToString();
-            int count = 0;
-
-            for (int i = 0; i < 10; i++)
-            {
-                count = 0;
-                string I = i.ToString();
-
-                for (int j = 0; j < X.Length; j++)
-                {
-                    int y = int.Parse(X);
-                    if (I[0] == X[j]) count++;
-                }
-
-                Console.WriteLine(count);
-            }
-        }
-
+    for (int j = 0; j < X.Length; j++)
+    {
+        if (I[0] == X[j]) count++;
     }
+
+    Console.WriteLine(count);
 }
