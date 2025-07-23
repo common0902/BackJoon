3710e4d63bb186a6990bcbd5b9d746ac32a3661d
common0902 <minsoung200@gmail.com>
2025-07-23 20:10:35 +0900

    [Silver IV] Title: 설탕 배달, Time: 52 ms, Memory: 5504 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cs
@@ -0,0 +1,23 @@
+int a = int.Parse(Console.ReadLine());
+
+int count = 0;
+
+bool can = true;
+
+while (true)
+{
+    if (a == 0) break;
+    
+    if (a < 0)
+    {
+        can = false;
+        break;
+    }
+
+    if (a % 5 == 0) a -= 5;
+    else a -= 3;
+
+    count++;
+}
+if(can) Console.WriteLine(count);
+else Console.WriteLine(-1);
