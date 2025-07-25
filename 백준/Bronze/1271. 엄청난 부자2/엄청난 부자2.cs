09c936ded5e56361e56ccc5ec582c45f8d5d36f5
common0902 <minsoung200@gmail.com>
2025-07-25 23:54:41 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 52 ms, Memory: 5688 KB -BaekjoonHub

--- 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.cs
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.cs
@@ -1,6 +1,6 @@
+using System.Data;
 using System.Numerics;
 using System.Runtime.Serialization;
-using System.Text;
 
 namespace ConsoleApp4
 {
@@ -11,6 +11,7 @@ namespace ConsoleApp4
             BigInteger[] ab = Array.ConvertAll(Console.ReadLine().Split(' '), BigInteger.Parse);
             BigInteger a = ab[0];
             BigInteger b = ab[1];
+
             Console.WriteLine(a / b);
             Console.WriteLine(a % b);
         }
