6d6b936517a254e114931d42b2dc6f4ecee4310a
common0902 <minsoung200@gmail.com>
2025-08-27 08:48:17 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 48 ms, Memory: 5352 KB -BaekjoonHub

--- 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cs
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cs
@@ -1,19 +1,3 @@
-using System.Globalization;
-using System.Text;
+string a = Console.ReadLine();
 
-namespace ConsoleApp4
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int[] arr1 = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
-            for (int i = 0; i < arr1[0]; i++)
-            {
-                int[] arr2 = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
-            }
-
-            Console.WriteLine("비와이");
-        }
-    }
-}
+Console.WriteLine("비와이");
