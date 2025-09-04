5cf7ce309f3422d0d2ca46b9cc72e27c521ebe49
common0902 <minsoung200@gmail.com>
2025-09-04 22:32:52 +0900

    [Bronze V] Title: SciComLove, Time: 48 ms, Memory: 5448 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cs
@@ -0,0 +1,12 @@
+int a = int.Parse(Console.ReadLine());
+
+switch (a)
+{
+    case 1:
+        Console.WriteLine("SciComLove");
+        break;
+    case 2:
+        Console.WriteLine("SciComLove");
+        Console.WriteLine("SciComLove");
+        break;
+}
