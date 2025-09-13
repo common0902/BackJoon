e5c7076f7743864c24aa91a27857aece362d85bd
common0902 <minsoung200@gmail.com>
2025-09-13 12:42:43 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 52 ms, Memory: 5468 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.cs
@@ -0,0 +1,9 @@
+int a = int.Parse(Console.ReadLine());
+
+if (a >= 620 && a <= 780) Console.WriteLine("Red");
+else if (a >= 590) Console.WriteLine("Orange");
+else if (a >= 570) Console.WriteLine("Yellow");
+else if (a >= 495) Console.WriteLine("Green");
+else if (a >= 450) Console.WriteLine("Blue");
+else if (a >= 425) Console.WriteLine("Indigo");
+else Console.WriteLine("Violet");
