1f7ef02d93f0ad02a0c1b38e9d39bc22394aac5c
common0902 <minsoung200@gmail.com>
2025-09-16 19:38:57 +0900

    [Bronze III] Title: 양수 개수 세기, Time: 284 ms, Memory: 80024 KB -BaekjoonHub

---
+++ 백준/Bronze/14909. 양수 개수 세기/양수 개수 세기.cs
@@ -0,0 +1,12 @@
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
