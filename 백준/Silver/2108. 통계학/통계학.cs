78125a2e00b3c284194236b726a3e4a2eb0fa88a
common0902 <minsoung200@gmail.com>
2025-09-27 11:43:14 +0900

    [Silver II] Title: 통계학, Time: 172 ms, Memory: 22760 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.cs
@@ -0,0 +1,49 @@
+int a = int.Parse(Console.ReadLine());
+
+List<int> list = new List<int>();
+
+long num = 0;
+
+for (int i = 0; i < a; i++)
+{
+    int b = int.Parse(Console.ReadLine());
+    list.Add(b);    
+    
+    num += b;
+}
+
+int c = (int)Math.Round((double)num / a, MidpointRounding.AwayFromZero);
+list.Sort();
+
+int[] arr = new int[8001];
+
+foreach (int i in list)
+{
+    arr[i + 4000]++;
+}
+
+int d = arr.Max();
+int e = 0;
+
+int num2 = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] != 0)
+    {
+        if (arr[i] == d)
+        {
+            num2++;
+            e = i - 4000;
+        }
+    }
+    if (num2 >= 2)
+    {
+        break;
+    }
+}
+
+Console.WriteLine(c); //1
+Console.WriteLine(list[list.Count / 2]); //2
+Console.WriteLine(e);
+Console.WriteLine(list.Max() - list.Min());
