2d5398c623bbda25db9caf5ba6e22fca2d0efd5d
common0902 <minsoung200@gmail.com>
2025-11-03 15:32:56 +0900

    [Bronze I] Title: FizzBuzz, Time: 52 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,26 @@
+int a = 0;
+for (int i = 3; i > 0; i--)
+{
+    string b = Console.ReadLine();
+    if (int.TryParse(b, out a))
+    {
+        a += i;
+        break;  
+    }
+}
+if (a % 5 == 0 && a % 3 == 0)
+{
+    Console.WriteLine("FizzBuzz");
+}
+else if (a % 3 == 0)
+{
+    Console.WriteLine("Fizz");
+}
+else if (a % 5 == 0)
+{
+    Console.WriteLine("Buzz");
+}
+else
+{
+    Console.WriteLine(a);
+}
