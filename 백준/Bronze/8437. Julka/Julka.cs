9453cf0b1cfcc4eb6ba1465f0a5b7f5c03b1ee91
common0902 <minsoung200@gmail.com>
2025-11-08 23:55:30 +0900

    [Bronze V] Title: Julka, Time: 48 ms, Memory: 5608 KB -BaekjoonHub

---
+++ 백준/Bronze/8437. Julka/Julka.cs
@@ -0,0 +1,8 @@
+
+
+using System.Numerics;
+BigInteger a = BigInteger.Parse(Console.ReadLine());
+BigInteger b = BigInteger.Parse(Console.ReadLine());
+
+Console.WriteLine((a + b) / 2);
+Console.WriteLine((a - b) / 2);
