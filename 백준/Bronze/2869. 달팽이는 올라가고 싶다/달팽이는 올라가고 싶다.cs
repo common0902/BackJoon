296f444f15fd0d1a8774f5d32556650036dff82f
common0902 <minsoung200@gmail.com>
2025-11-17 17:09:33 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 48 ms, Memory: 5500 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cs
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cs
@@ -1,25 +1,2 @@
-int[] arr = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
-int a = arr[0];
-int b = arr[1];
-int c = arr[2];
-
-c -= a;
-int count = 1;
-
-if (a - b != 1)
-{
-    while (true)
-    {
-        if (c <= 0) break;
-
-        c -= (a - b);
-
-        count++;
-    }
-}
-else
-{
-    count += c / (a - b);
-}
-
-Console.WriteLine(count);
+ string[] ss = Console.ReadLine().Split();
+Console.Write((int.Parse(ss[2])-int.Parse(ss[1])-1)/(int.Parse(ss[0])-int.Parse(ss[1]))+1);
