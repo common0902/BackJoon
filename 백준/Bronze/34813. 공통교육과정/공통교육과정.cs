04a45b2f929792390d2d549867f318c79361a4b3
common0902 <minsoung200@gmail.com>
2025-12-15 23:52:01 +0900

    [Bronze V] Title: 공통교육과정, Time: 48 ms, Memory: 5368 KB -BaekjoonHub

---
+++ 백준/Bronze/34813. 공통교육과정/공통교육과정.cs
@@ -0,0 +1,6 @@
+string a = Console.ReadLine();
+
+if (a[0] == 'F') Console.WriteLine("Foundation");
+else if (a[0] == 'C') Console.WriteLine("Claves");
+else if (a[0] == 'V') Console.WriteLine("Veritas");
+else if (a[0] == 'E') Console.WriteLine("Exploration");
