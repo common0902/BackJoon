2fd3936e7dd31893e469e6925398ecea72a7c94a
common0902 <minsoung200@gmail.com>
2026-02-25 00:13:58 +0900

    [Bronze V] Title: Робинзон Крузо, Time: 100 ms, Memory: 5492 KB -BaekjoonHub

---
+++ 백준/Bronze/27219. Робинзон Крузо/Робинзон Крузо.cs
@@ -0,0 +1,10 @@
+int a = int.Parse(Console.ReadLine());
+
+int num = a / 5;
+int num2 = a % 5;
+
+for (int i = 0; i < num; i++)
+    Console.Write("V");
+
+for (int i = 0; i < num2; i++)
+    Console.Write("I");
