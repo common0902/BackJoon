856838c6941781bbc5b5153baee292f8f2661dec
common0902 <minsoung200@gmail.com>
2026-03-15 22:42:05 +0900

    [Bronze V] Title: ブラックジャック (Blackjack), Time: 48 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/33170. ブラックジャック （Blackjack）/ブラックジャック （Blackjack）.cs
@@ -0,0 +1,9 @@
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+
+if(a + b + c <= 21 )
+    Console.WriteLine(1);
+else
+    Console.WriteLine(0);
