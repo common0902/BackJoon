218437cbce4dd6d41a7c15b0a35f3c43893f5319
common0902 <minsoung200@gmail.com>
2026-04-02 16:13:53 +0900

    [Bronze IV] Title: 관공... 어찌하여 목만 오셨소..., Time: 48 ms, Memory: 5584 KB -BaekjoonHub

--- 백준/Bronze/30501. 관공．．． 어찌하여 목만 오셨소．．．/관공．．． 어찌하여 목만 오셨소．．．.cs
+++ 백준/Bronze/30501. 관공．．． 어찌하여 목만 오셨소．．．/관공．．． 어찌하여 목만 오셨소．．．.cs
@@ -3,6 +3,9 @@ int a = int.Parse(Console.ReadLine());
 for (int i = 0; i < a; i++)
 {
     string b = Console.ReadLine();
-    if(b.Contains('S'))
+    if (b.Contains('S'))
+    {
         Console.WriteLine(b);
+        break;
+    }
 }
