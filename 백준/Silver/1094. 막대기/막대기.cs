35b0040166053431d96574c63e6b4eaf8b28fe7a
common0902 <minsoung200@gmail.com>
2026-04-05 12:20:59 +0900

    [Silver V] Title: 막대기, Time: 48 ms, Memory: 5512 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.cs
@@ -0,0 +1,16 @@
+int a = int.Parse(Console.ReadLine());
+int[] arr = { 64, 32, 16, 8, 4, 2, 1 };
+int b = 0;
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    b += arr[i];
+    count++;
+    if (b == a) break;
+    else if (b > a)
+    {
+        count--;
+        b -= arr[i];
+    }
+}
+Console.WriteLine(count);
