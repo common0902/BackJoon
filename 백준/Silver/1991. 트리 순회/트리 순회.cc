ed3fa185315030ed96a071417a3a7dd139335ddf
common0902 <minsoung200@gmail.com>
2026-04-10 08:43:42 +0900

    [Silver I] Title: 트리 순회, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+void preorder(char ch);
+void inorder(char ch);
+void postorder(char postorder);
+map<char, pair<char, char>>node;
+
+int main()
+{
+	int n;
+	char lname, name, rname;
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> name >> lname >> rname;
+		node[name] = { lname,rname };
+	}
+	preorder('A'); cout << "\n";
+	inorder('A'); cout << "\n";
+	postorder('A'); cout << "\n";
+
+}
+
+void preorder(char ch)
+{
+	if (ch == '.') return;
+	cout << ch;
+	preorder(node[ch].first);
+	preorder(node[ch].second);
+}
+
+void inorder(char ch)
+{
+	if (ch == '.') return;
+	inorder(node[ch].first);
+	cout << ch;
+	inorder(node[ch].second);
+}
+
+void postorder(char postorder)
+{
+	if (postorder == '.') return;
+	::postorder(node[postorder].first);
+	::postorder(node[postorder].second);
+	cout << postorder;
+}
