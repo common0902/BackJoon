61419f3eaaac7681cab110dcb91dfacabfac522e
common0902 <minsoung200@gmail.com>
2026-04-16 00:44:38 +0900

    [level 0] Title: [PCCE 기출문제] 1번 / 문자 출력, Time: 2.00 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/0/340207. ［PCCE 기출문제］ 1번 ／ 문자 출력/［PCCE 기출문제］ 1번 ／ 문자 출력.cpp
+++ 프로그래머스/0/340207. ［PCCE 기출문제］ 1번 ／ 문자 출력/［PCCE 기출문제］ 1번 ／ 문자 출력.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
     string message = "Let's go!";
 
-    cout << "3"<< '\n' <<"2"<< '\n' <<"1" << endl;
+    cout << "3\n2\n1" << endl;
     cout << message << endl;
 
     return 0;
