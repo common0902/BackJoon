bb02c7546bbfc5ccf2d3cb2705fc7ee3bd8b26c7
common0902 <minsoung200@gmail.com>
2026-05-11 08:44:31 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.42 ms, Memory: 30.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string, int k) {
+        string answer = "";
+        for (int i = 0; i < k; ++i) answer += my_string;
+        return answer;
+    }
+}
