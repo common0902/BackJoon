ac2f2403716153290cde5022b032908a2d88a73c
common0902 <minsoung200@gmail.com>
2026-06-09 08:53:02 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.16 ms, Memory: 30.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(int[] num_list) {
+        int a = 0;
+        int b = 1;
+        for(int i = 0; i < num_list.Length; ++i)
+        {
+            a += num_list[i];
+            b *= num_list[i];
+        }
+        return a * a > b ? 1 : 0;
+    }
+}
