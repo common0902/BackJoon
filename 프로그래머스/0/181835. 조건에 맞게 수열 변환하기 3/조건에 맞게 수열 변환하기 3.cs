3cafaf779751b307cd3b9d6cc151763bda88d6c3
common0902 <minsoung200@gmail.com>
2026-06-13 11:03:12 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 3.37 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] arr, int k) {
+        int[] answer = new int[arr.Length];
+        for(int i = 0; i < arr.Length; ++i)
+        {
+            answer[i] = k % 2 == 0 ? arr[i] + k : arr[i] * k; 
+        }
+        return answer;
+    }
+}
