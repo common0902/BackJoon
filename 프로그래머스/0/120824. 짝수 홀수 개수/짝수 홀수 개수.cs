96cd30f349d43a1205004f42941af2fe3eab7c52
common0902 <minsoung200@gmail.com>
2026-06-27 23:59:23 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.10 ms, Memory: 46.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] num_list) {
+        int even = 0;
+        int odd = 0;
+
+        for (int i = 0; i < num_list.Length; i++)
+        {
+            if (num_list[i] % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+
+        int[] answer = new int[2];
+        answer[0] = even;
+        answer[1] = odd;
+
+        return answer;
+    }
+}
