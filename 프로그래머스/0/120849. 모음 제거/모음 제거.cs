da4efe7bef7f13d6b076815582f6d4cdd2bf3257
common0902 <minsoung200@gmail.com>
2026-08-26 00:08:01 +0900

    [level 0] Title: 모음 제거, Time: 0.07 ms, Memory: 46.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string) {
+        return my_string.Replace("a","").Replace("i","").Replace("o","").Replace("e","").Replace("u","");
+    }
+}
