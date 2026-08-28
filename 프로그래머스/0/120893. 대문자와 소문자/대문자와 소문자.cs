f94679a54d192e35095b108614dfd6260191ca48
common0902 <minsoung200@gmail.com>
2026-08-28 22:36:55 +0900

    [level 0] Title: 대문자와 소문자, Time: 8.22 ms, Memory: 49.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string) {
+        string answer = "";
+        foreach (char c in my_string)
+        {
+            
+            if (char.IsUpper(c)) answer += char.ToLower(c);
+            else answer += char.ToUpper(c);
+        }
+        return answer;
+    }
+}
