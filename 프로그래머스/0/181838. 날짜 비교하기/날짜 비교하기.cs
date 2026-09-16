04c4f628885511582a11ed71b0c42ef7da204471
common0902 <minsoung200@gmail.com>
2026-09-16 14:34:11 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.15 ms, Memory: 46.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public int solution(int[] date1, int[] date2) {
+        DateTime d1 = new DateTime(date1[0], date1[1], date1[2]);
+        DateTime d2 = new DateTime(date2[0], date2[1], date2[2]);
+        
+        return d1 < d2 ? 1 : 0;
+    }
+}
